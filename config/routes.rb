92ca802cacb8dbd587b1336e956f611fdce6a642
Pedro <mendez.pedro94@gmail.com>
2021-07-24 18:46:08 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "users/login"
   get  "pages/about_us"
-  root "pages#index"
+  get "home/index"
+  root "home#index"
 end
